9d7b2d5e67e9de3452ed9a31ed426440378d4c3d
dmorad1004 <dmorad@unal.edu.co>
2020-11-23 16:42:42 -0500

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -23,7 +23,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Nihil:jB2gCiJy4HWjTlU2@cluster0.nspak.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 
